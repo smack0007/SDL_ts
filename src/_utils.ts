012f32c7c702e840144521c4506734efba3bfff4
smack0007 <zachary.snow@gmail.com>
2022-06-10 15:22:09 +0200

    Move some lines around.

--- src/_utils.ts
+++ src/_utils.ts
@@ -2,6 +2,16 @@
 // exposed as part of the API.
 
 //
+// Constants
+//
+
+export const ENDIANNESS = (function (): "BE" | "LE" {
+  const buffer = new ArrayBuffer(2);
+  new globalThis.DataView(buffer).setInt16(0, 256, true);
+  return new Int16Array(buffer)[0] === 256 ? "LE" : "BE";
+})();
+
+//
 // Types
 //
 
@@ -11,8 +21,4 @@ export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 // Functions
 //
 
-export const ENDIANNESS = (function (): "BE" | "LE" {
-  const buffer = new ArrayBuffer(2);
-  new globalThis.DataView(buffer).setInt16(0, 256, true);
-  return new Int16Array(buffer)[0] === 256 ? "LE" : "BE";
-})();
+
