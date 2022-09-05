7e5ed8078fd308fe3d21a47e5a8497689963ec30
smack0007 <zachary.snow@gmail.com>
2022-09-05 20:19:12 +0200

    Add colors to build script.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,10 @@
 import { ROOT_DIRECTORY } from "./config.ts";
-import { path } from "./deps.ts";
+import { colors, path } from "./deps.ts";
 
 for await (const entry of Deno.readDir(path.join(ROOT_DIRECTORY, "examples"))) {
   if (entry.isDirectory) {
     const mainFilePath = path.join(ROOT_DIRECTORY, "examples", entry.name, "main.ts");
-    console.info(`Type checking: ${mainFilePath}`);
+    console.info(`${colors.green("Type checking:")} ${mainFilePath}`);
 
     await Deno.run({
       cmd: ["deno", "task", "-q", "check", mainFilePath],
--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,1 +1,2 @@
+export * as colors from "https://deno.land/std@0.154.0/fmt/colors.ts";
 export * as path from "https://deno.land/std@0.154.0/path/mod.ts";
