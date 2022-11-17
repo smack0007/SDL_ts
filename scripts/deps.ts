9281b518e78e9a7a96e2df83fff53fa9fc0f6037
smack0007 <zachary.snow@gmail.com>
2022-11-17 09:37:55 +0100

    Update scripts/deps.ts

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,2 @@
-export * as colors from "https://deno.land/std@0.154.0/fmt/colors.ts";
-export * as path from "https://deno.land/std@0.154.0/path/mod.ts";
+export * as colors from "https://deno.land/std@0.165.0/fmt/colors.ts";
+export * as path from "https://deno.land/std@0.165.0/path/mod.ts";
