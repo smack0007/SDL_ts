e2163dd4546ac0e2d346bdecd1068bff57bd99d0
smack0007 <zachary.snow@gmail.com>
2022-11-17 09:18:15 +0100

    Build should fail now when a type check fails.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,21 @@
 import { ROOT_DIRECTORY } from "./config.ts";
 import { colors, path } from "./deps.ts";
 
+let failure = false;
+
 for await (const entry of Deno.readDir(path.join(ROOT_DIRECTORY, "examples"))) {
   if (entry.isDirectory) {
     const mainFilePath = path.join(ROOT_DIRECTORY, "examples", entry.name, "main.ts");
     console.info(`${colors.green("Type checking:")} ${mainFilePath}`);
 
-    await Deno.run({
+    const status = await Deno.run({
       cmd: ["deno", "task", "-q", "check", mainFilePath],
     }).status();
+
+    if (status.code !== 0) {
+      failure = true;
+    }
   }
 }
+
+Deno.exit(failure ? 1 : 0);
