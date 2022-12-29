8b65ed1eca2a792c3282d62b55665d74a4665675
smack0007 <zachary.snow@gmail.com>
2022-12-29 11:01:21 +0100

    loadLibrary will now attempt to load a fallback lirbrary if loading the specified library does not succeed.

--- src/deno/_library.ts
+++ src/deno/_library.ts
@@ -1,27 +1,59 @@
-import { join } from "std/path/mod.ts";
+import { basename, join } from "std/path/mod.ts";
 import { SDLError } from "../error.ts";
 import { DynamicLibrary, DynamicLibraryInterface } from "../_library.ts";
 import { ENV_LIBRARY_PATH } from "../_constants.ts";
 
 const IS_WINDOWS = Deno.build.os === "windows";
 
+function getLibrarySuffix(): string {
+  switch (Deno.build.os) {
+    case "windows":
+      return ".dll";
+
+    case "darwin":
+      return ".dylib";
+  }
+
+  return ".so";
+}
+
 export function getLibraryPath(libraryName: string): string {
-  const libraryPath = Deno.env.get(ENV_LIBRARY_PATH) ?? ".";
+  const libraryPath = Deno.env.get(ENV_LIBRARY_PATH);
+
+  if (!libraryPath) {
+    return libraryName;
+  }
+
   const libraryPrefix = !IS_WINDOWS ? "lib" : "";
-  const libraryExtension = IS_WINDOWS ? ".dll" : ".a";
+  const librarySuffix = getLibrarySuffix();
 
   return join(
     libraryPath,
     Deno.build.os,
     "x64",
-    libraryPrefix + libraryName + libraryExtension,
+    libraryPrefix + libraryName + librarySuffix,
   );
 }
 
 export function loadLibrary<T>(libraryPath: string, symbols: DynamicLibraryInterface): DynamicLibrary<T> {
+  let loadError: Error | undefined;
+
   try {
     return Deno.dlopen(libraryPath, symbols as Deno.ForeignLibraryInterface) as unknown as DynamicLibrary<T>;
   } catch (error) {
-    throw new SDLError(`Failed to load library "${libraryPath}"`, error);
+    loadError = error;
+
+    // If the basename is not the same as the given path then attempt
+    // to load a fallback library.
+    const libraryBasename = basename(libraryPath);
+    if (libraryBasename != libraryPath) {
+      try {
+        return Deno.dlopen(libraryPath, symbols as Deno.ForeignLibraryInterface) as unknown as DynamicLibrary<T>;
+      } catch {
+        // Just ignore the error if we can't load the fallback library.
+      }
+    }
   }
+
+  throw new SDLError(`Failed to load library "${libraryPath}"`, loadError);
 }
