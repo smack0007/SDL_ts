76496cb4c4a8511bff6b136c88c5142f303c5345
jingkaimori <jingkaimori@gmail.com>
2023-07-17 20:52:15 +0800

    revert complicated type inferring from DynamicLibrary

--- src/_library.ts
+++ src/_library.ts
@@ -1,54 +1,27 @@
-import { PlatformPointer, PlatformString } from "./_types.ts";
-type DynamicLibraryStructType = { readonly struct: Readonly<Array<DynamicLibraryType>> };
-type DynamicLibraryTypeMap = {
-  "bool": boolean;
-  "i8": number;
-  "u8": number;
-  "i16": number;
-  "u16": number;
-  "i32": number;
-  "u32": number;
-  "i64": number | bigint;
-  "u64": number | bigint;
-  "f32": number;
-  "f64": number;
-  "pointer": PlatformPointer<unknown> | PlatformString | null;
-  "void": void;
-};
-export type DynamicLibraryType = Exclude<keyof DynamicLibraryTypeMap, "void"> | DynamicLibraryStructType;
-export type DynamicLibraryResultType = keyof DynamicLibraryTypeMap | DynamicLibraryStructType;
+export type DynamicLibraryType =
+  | "bool"
+  | "i8"
+  | "u8"
+  | "i16"
+  | "u16"
+  | "i32"
+  | "u32"
+  | "i64"
+  | "u64"
+  | "f32"
+  | "f64"
+  | "pointer"
+  | { struct: Readonly<Array<DynamicLibraryType>> };
 
 export type DynamicLibraryFunctionInterface = {
   readonly parameters: readonly DynamicLibraryType[];
-  readonly result: DynamicLibraryResultType;
+  readonly result: DynamicLibraryType | "void";
 };
 
 export type DynamicLibraryInterface = Record<string, DynamicLibraryFunctionInterface>;
 
-type ToNativeType<T extends DynamicLibraryType = DynamicLibraryType> = T extends DynamicLibraryStructType ? BufferSource
-  : DynamicLibraryTypeMap[Exclude<T, DynamicLibraryStructType>];
-type StaticForeignSymbolReturnType<T extends DynamicLibraryFunctionInterface> = T["result"] extends
-  DynamicLibraryStructType ? Uint8Array
-  : DynamicLibraryTypeMap[Exclude<T["result"], DynamicLibraryStructType>];
-type ToNativeParameterTypes<T extends readonly DynamicLibraryType[]> =
-  //
-  [(T[number])[]] extends [T] ? ToNativeType<T[number]>[]
-    : [readonly (T[number])[]] extends [T] ? readonly ToNativeType<T[number]>[]
-    : T extends readonly [...DynamicLibraryType[]] ? {
-        [K in keyof T]: ToNativeType<T[K]>;
-      }
-    : never;
-type FromForeignFunction<T extends DynamicLibraryFunctionInterface> = T["parameters"] extends readonly []
-  ? () => StaticForeignSymbolReturnType<T>
-  : (
-    ...args: ToNativeParameterTypes<T["parameters"]>
-  ) => StaticForeignSymbolReturnType<T>;
-type StaticForeignLibraryInterface<T extends DynamicLibraryInterface> = {
-  [K in keyof T]: FromForeignFunction<T[K]>;
-};
-
-export type DynamicLibrary<T extends DynamicLibraryInterface> = {
-  symbols: StaticForeignLibraryInterface<T>;
+export type DynamicLibrary<T> = {
+  symbols: Record<keyof T, (...args: unknown[]) => unknown>;
 
   close(): void;
 };
