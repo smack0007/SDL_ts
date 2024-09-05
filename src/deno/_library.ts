a1610614c8d192fdcc270c85cd9d838673bca15e
Zachary Snow <zachary.snow@gmail.com>
2024-09-05 21:24:01 +0200

    Refactored getLibraryPaths

--- src/deno/_library.ts
+++ src/deno/_library.ts
@@ -1,4 +1,4 @@
-import { join } from "@std/path";
+import { join, SEPARATOR } from "@std/path";
 import { SDLError } from "../error.ts";
 import { DynamicLibrary, DynamicLibraryInterface } from "../_library.ts";
 import { ENV_LIBRARY_PATH } from "../_constants.ts";
@@ -11,7 +11,7 @@ const WINDOWS_LIBRARY_PATHS: string[] = [];
 const UNIX_LIBRARY_PATHS: string[] = [
   "/usr/local/lib",
   "/usr/lib64",
-  "/home/linuxbrew/.linuxbrew/lib"
+  "/home/linuxbrew/.linuxbrew/lib",
 ];
 
 const MACOS_LIBRARY_PATHS: string[] = [
@@ -31,33 +31,32 @@ function getLibrarySuffix(): string {
   return ".so";
 }
 
-function getLibraryPaths(libraryName: string, libraryPath?: string): string[] {
+function getLibraryPaths(libraryName: string): string[] {
   const libraryPrefix = !IS_WINDOWS ? "lib" : "";
   const librarySuffix = getLibrarySuffix();
   const fullLibraryName = libraryPrefix + libraryName + librarySuffix;
 
   const libraryPaths: string[] = [];
 
-  const os = Deno.build.os;
-  const arch = "x64"; // TODO: Detect this somehow.
+  // Try and load it from the same directory.
+  // NOTE: Using `join` here doesn't seem to work as it was just returning the file without the ./
+  libraryPaths.push(`.${SEPARATOR}${fullLibraryName}`);
 
-  if (libraryPath) {
-    libraryPaths.push(join(libraryPath, os, arch, fullLibraryName));
+  // Then look and see if SDL_TS_LIBRARY_PATH was specified.
+  const envLibraryPath = Deno.env.get(ENV_LIBRARY_PATH);
+  if (envLibraryPath) {
+    libraryPaths.push(join(envLibraryPath, fullLibraryName));
   }
 
-  libraryPaths.push(join(".", fullLibraryName));
+  // Next try and load it globally
+  libraryPaths.push(fullLibraryName);
 
   if (!IS_WINDOWS && !IS_MAC) {
-    // On Debain libSDL2_image and libSDL2_ttf only have symbolic links with this format.
+    // On Debain libSDL2_image and libSDL2_ttf only have symbolic links with this format so
+    // search for those globally as well.
     libraryPaths.push(
       libraryPrefix + libraryName + "-2.0" + librarySuffix + ".0"
     );
-  }
-
-  libraryPath = Deno.env.get(ENV_LIBRARY_PATH);
-
-  if (libraryPath) {
-    libraryPaths.push(join(libraryPath, os, arch, fullLibraryName));
   }
 
   if (!IS_WINDOWS) {
@@ -100,14 +99,21 @@ export function denoLoadLibrary<T extends DynamicLibraryInterface>(
   symbols: T,
   libraryPath?: string
 ): DynamicLibrary<T> {
-  const libraryPaths = getLibraryPaths(libraryName, libraryPath);
+  const libraryPaths = libraryPath
+    ? [libraryPath]
+    : getLibraryPaths(libraryName);
   const errors: Record<string, Error> = {};
 
   for (const libraryPath of libraryPaths) {
     try {
-      // Cast the symbols as any in order to prevent a type checking bug.
-      // deno-lint-ignore no-explicit-any
-      return Deno.dlopen(libraryPath, symbols as any) as DynamicLibrary<T>;
+      const result = Deno.dlopen(
+        libraryPath,
+        // Cast the symbols as any in order to prevent a type checking bug.
+        // deno-lint-ignore no-explicit-any
+        symbols as any
+      ) as DynamicLibrary<T>;
+      console.debug(`SDL_ts: Loaded ${libraryName} from "${libraryPath}"`);
+      return result;
     } catch (error) {
       errors[libraryPath] = error;
     }
